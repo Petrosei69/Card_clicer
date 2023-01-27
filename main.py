87ceb2540c0dec5ab124eb068e4c231c28526799
Petrosei69 <t-m-e@mail.ru>
2023-01-27 23:19:58 +0600

    first normal version commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# Cleaner file to the strings
-
 #  empty line cleaner
-with open('file.txt') as f:
+with open('file.txt') as f:     # enter your filename
     lines = f.readlines()
     non_empty_lines = (line for line in lines if not line.isspace())
     with open('new_file.txt', 'w') as n_f:
@@ -9,13 +7,11 @@ with open('file.txt') as f:
 
 #  space cleaner
 def read2list(file):
-    # открываем файл в режиме чтения utf-8
     file = open('new_file.txt', 'r', encoding='utf-8')
-
-    # читаем все строки и удаляем переводы строк
     lines = file.readlines()
     lines = [line.rstrip('\n') for line in lines]
     return lines
+
 lines = read2list('new_file.txt')
 
 
@@ -25,53 +21,58 @@ for i in range(len(lines)):
 with open('new_file.txt','w', encoding="utf-8") as tfile:
 	tfile.write('\n'.join(lines))
 
-file = open('new_file.txt', "r")
-all_words = []
-line = file.readline().split()
-while line:
-    all_words.extend(line)
+# part of creating new files:  1.sequence with hidden numbers  2.hidden numbers only
+
+def cutter(new_file):
+    file = open('new_file.txt', "r")
+    all_words = []
     line = file.readline().split()
+    while line:
+        all_words.extend(line)
+        line = file.readline().split()
 
-arr1 = []
-result_list = []
+    arr1 = []
 
+    for i in range(len(all_words)):
+        arr2 = []
+        for x in all_words[i]:
+            arr2.append(x)
+        arr1.append(arr2)
 
-
-for i in range(len(all_words)):
     arr2 = []
-    for x in all_words[i]:
-        arr2.append(x)
-    arr1.append(arr2)
+    for i in range(len(all_words)):
+        arr3 = []
+        for k in range(6):
+            arr3.append(arr1[i][k + 6])
+            arr1[i].pop(k + 6, )
+            arr1[i].insert(k + 6, '*')
+        arr2.append(arr3)
 
-arr2 = []
-for i in range(len(all_words)):
-    arr3 = []
-    for k in range(6):
-        arr3.append(arr1[i][k + 6])
-        arr1[i].pop(k + 6,)
-        arr1[i].insert(k + 6, '*')
-    arr2.append(arr3)
+    for i in range(len(arr1)):
+        arr1[i].insert(4, ' ')
+        arr1[i].insert(9, ' ')
+        arr1[i].insert(14, ' ')
 
-for i in range(len(arr1)):
-    arr1[i].insert(4, ' ')
-    arr1[i].insert(9, ' ')
-    arr1[i].insert(14, ' ')
+    arr4 = []
+    arr5 = []
 
-print(arr1[0][0])
+    for i in range(len(arr1)):
+        s = ''.join(map(str, arr1[i]))
+        arr4.append(s)
 
+    with open('array1.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % place for place in arr4)
 
-arr4 = []
-arr5 = []
+    for i in range(len(arr2)):
+        q = ''.join(map(str, arr2[i]))
+        arr5.append(q)
 
-for i in range(len(arr1)):
-    s = ''.join(map(str, arr1[i]))
-    arr4.append(s)
+    with open('array2.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % place for place in arr5)
 
+    # print(f"arr1 ={arr1}")
+    # print(f"arr2 ={arr2}")
+    # print(f"arr4 ={arr4}")
+    # print(f"arr5 ={arr5}")
 
-with open('array1.txt', 'w') as filehandle: filehandle.writelines("%s\n" % place for place in arr4)
-
-for i in range(len(arr2)):
-    q = ''.join(map(str, arr2[i]))
-    arr5.append(q)
-
-with open('array2.txt', 'w') as filehandle: filehandle.writelines("%s\n" % place for place in arr5)
+cutter('new_file.txt')
